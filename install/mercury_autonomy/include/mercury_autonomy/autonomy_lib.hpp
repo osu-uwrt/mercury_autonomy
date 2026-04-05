19c9a02470249c594314961ba1c82fded0499305
AdityaGupta0 <bobdobchob@gmail.com>
2026-04-05 15:29:36 -0400

    im dumb

--- install/mercury_autonomy/include/mercury_autonomy/autonomy_lib.hpp
+++
@@ -1,8 +0,0 @@
-// Convenience aggregate header for the mercury_autonomy library.
-// Include this in custom node headers and source files.
-
-#pragma once
-
-#include "mercury_autonomy/autonomy_base.hpp"
-#include "mercury_autonomy/mercury_bt_node.hpp"
-#include "mercury_autonomy/autonomy_util.hpp"
